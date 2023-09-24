6723f5483f4e89da39dec5d1587756113572004a
anhtuan0302 <anhtuan030203@gmail.com>
2023-09-24 17:35:42 +0700

    c04 - Practice

---
+++ 1651 Adv Prog/c04 - Practice/Ex1 - Temperature/1 - Temperature.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1651_Adv_Prog.c04___Practice.Ex1___Temperature
+{
+    public class Temperature
+    {
+        private double _ftemp;
+
+        public double FTemp
+        {
+            get { return _ftemp; }
+            set { _ftemp = value; }
+        }
+
+        public Temperature(): this(0)
+        {
+        }
+
+        public Temperature(double ftemp)
+        {
+            this.FTemp = ftemp;
+        }
+
+        public double getCelcius()
+        {
+            return (double) 5/9 * (FTemp - 32);
+        }
+
+        public double getKelvin()
+        {
+            return getCelcius() + 273;
+        }
+
+    }
+}
---
+++ 1651 Adv Prog/c04 - Practice/Ex1 - Temperature/Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1651_Adv_Prog.c04___Practice.Ex1___Temperature
+{
+    public class Test
+    {
+        public static void Main(string[] args)
+        {
+            double ftemp;
+            while (true)
+            {
+                Console.Write("Please enter Fahrenheit value: ");
+                ftemp = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine();
+
+                if (ftemp == 0)
+                    break;
+
+                else
+                {
+                    Temperature temp = new Temperature(ftemp);
+                    Console.WriteLine("Fahrenheit: " + temp.FTemp);
+                    Console.WriteLine("Celcius: " + temp.getCelcius());
+                    Console.WriteLine("Kelvin: " + temp.getKelvin());
+
+                    Console.WriteLine();
+                    Console.WriteLine("*********************************");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
